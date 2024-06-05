3010f1de45915b132c38f6014115a4c196f8102f
Lar97 <dr.number@yandex.ru>
2024-06-05 19:48:53 +0700

    correct coding

--- lab4/lab4.cpp
+++ lab4/lab4.cpp
@@ -5,118 +5,115 @@
 #include <vector>
 #include <windows.h>
 #include <algorithm>
-#include <locale>
-#include <codecvt>
 
 using namespace std;
 
 int main()
 {
-    // Устанавливаем кодировку консоли на Windows-1251
-    SetConsoleCP(1251);
-    SetConsoleOutputCP(1251);
+    setlocale(LC_ALL, "ru_RU.UTF-8");
+    SetConsoleCP(CP_UTF8);
+    SetConsoleOutputCP(CP_UTF8);
 
-    // Устанавливаем локаль для wcin и wcout
-    locale loc(".1251");
-    wcin.imbue(loc);
-    wcout.imbue(loc);
+    map <char, int> bukvi;
+    map <int, char> bukvi1;
 
-    // Карты для соответствия русских букв и их индексов
-    map<wchar_t, int> bukvi;
-    map<int, wchar_t> bukvi1;
-
-    // Инициализация карт
-    wchar_t letters[] = L"АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
-    int n = wcslen(letters);
-
-    for (int i = 0; i < n; i++) {
-        bukvi[letters[i]] = i;
-        bukvi1[i] = letters[i];
+    for (int i = 0; i < 6; i++) {
+        bukvi[char(int('А') + i)] = i;
+        bukvi1[i] = char(int('А') + i);
     }
+    bukvi['Ё'] = 6;
+    bukvi1[6] = 'Ё';
+    for (int i = 7; i < 33; i++) {
+        bukvi[char(int('Ж') + i - 7)] = i;
+        bukvi1[i] = char(int('Ж') + i - 7);
+    }
+    //for (auto b : bukvi) {
+    //    cout << b.first << " " << b.second << endl;
+    //}
 
-    // Пути к файлам
-    const wstring PATH = L"D:\\СИБСТРИН ИНФА\\семестр6\\2324, Теория информации, данные, знания, 12 (экзамен, заоч. (5, 6 сем.) (310а-з, 310з гр.) )\\Лабораторная работа №4\\";
-    const wstring INPUT = L"Вариант 17.txt";
+    const string PATH = "D:\\СИБСТРИН ИНФА\\семестр6\\2324, Теория информации, данные, знания, 12 (экзамен, заоч. (5, 6 сем.) (310а-з, 310з гр.) )\\Лабораторная работа №4\\";
+    const string INPUT = "Вариант 17_.txt";
 
-    wstring pathin = PATH + INPUT;
-    wstring pathout = PATH + INPUT + L"_output.txt";
-    wstring pathout2 = PATH + INPUT + L"_recovery.txt";
+    string pathin = PATH + INPUT;
+    string pathout = PATH + INPUT + "_output.txt";
+    string pathout2 = PATH + INPUT + "_recovery.txt";
 
     // Открытие файлов
-    wifstream fin(pathin);
-    wofstream fout(pathout);
-    wofstream fout2(pathout2);
+    ifstream fin(pathin);
+    ofstream fout(pathout);
+    ofstream fout2(pathout2);
 
-    // Устанавливаем кодировку для файлов
-    fin.imbue(locale(fin.getloc(), new codecvt_utf8<wchar_t>));
-    fout.imbue(locale(fout.getloc(), new codecvt_utf8<wchar_t>));
-    fout2.imbue(locale(fout2.getloc(), new codecvt_utf8<wchar_t>));
+    fin.open(pathin);
+    fout.open(pathout);
+    fout2.open(pathout2);
 
-    if (!fin.is_open() || !fout.is_open()) {
-        wcerr << L"Не удалось открыть файл" << endl;
-        return 1;
-    }
+    cout << "Исходный текст : \n";
+    vector <char> t_in;
+    char ch;
 
-    vector<wchar_t> t_in;
-    wchar_t ch;
-
-    wcout << L"Исходный текст:\n";
-
-    // Чтение входного файла
-    while (fin.get(ch)) {
+    while (fin.get(ch))
+    {
         t_in.push_back(ch);
-        wcout << ch;
+        cout << ch;
     }
 
-    // Ввод ключевого слова (гаммы)
-    wcout << L"\nВведите ключевое слово (гамму): ";
-    wstring str1;
-    wcin >> str1;
+    cout << "Введите ключевое слово (гамму)  ";
+    string str1;
 
-    map<int, wchar_t> key;
+    cin >> str1;
+
+    map <int, char> key;
     int kol_el = str1.length();
 
-    for (int i = 0; i < kol_el; i++) {
+    for (int i = 0; i < kol_el; i++)
+    {
         key[i] = str1[i];
     }
 
-    vector<wchar_t> t_out;
+    vector <char> t_out;
+
     int j = 0;
 
-    for (int i = 0; i < t_in.size(); i++) {
-        bool a = t_in[i] >= L'А';
-        bool b = t_in[i] <= L'Я';
-        bool c = t_in[i] == L'Ё';
-        if ((a && b) || c) {
-            wchar_t ind = bukvi1[(bukvi[t_in[i]] + bukvi[key[j]]) % 33];
+    for (int i = 0; i < t_in.size(); i++)
+    {
+        bool a = t_in[i] >= 'А';
+        bool b = t_in[i] <= 'Я';
+        bool c = t_in[i] == 'Ё';
+        if ((a and b) or c)
+        {
+            char ind = bukvi1[(bukvi[t_in[i]] + bukvi[key[j]]) % 33];
             t_out.push_back(ind);
-            if (j == kol_el - 1) {
-                j = 0;
-            }
-            else {
-                j++;
-            }
+            if (j == kol_el - 1) { j = 0; }
+            else { j++; }
         }
-        else {
+        else
+        {
             t_out.push_back(t_in[i]);
         }
+
     }
 
-    wcout << L"\nЗашифрованный текст:\n";
+    for (int i = 0; i < t_out.size(); i++)
+    {
+        cout << t_out[i];
+        fout << t_out[i];
+    }
+
+    cout << L"\nЗашифрованный текст:\n";
     // Вывод результатов
     for (int i = 0; i < t_out.size(); i++) {
-        wcout << t_out[i];
+        cout << t_out[i];
         fout << t_out[i];
     }
 
     vector<wchar_t> t_out2;
     j = 0;
 
-    wcout << L"\nВновь расшифрованный текст:\n";
+    cout << L"\nВновь расшифрованный текст:\n";
     for (int i = 0; i < t_out.size(); i++) {
-        bool a = t_out[i] >= L'А';
-        bool b = t_out[i] <= L'Я';
-        bool c = t_out[i] == L'Ё';
+        bool a = t_out[i] >= 'А';
+        bool b = t_out[i] <= 'Я';
+        bool c = t_out[i] == 'Ё';
         if ((a && b) || c) {
             wchar_t ind = bukvi1[(bukvi[t_out[i]] - bukvi[key[j]] + 33) % 33];
             t_out2.push_back(ind);
@@ -133,9 +130,8 @@ int main()
     }
 
     for (int i = 0; i < t_out2.size(); i++) {
-        wcout << t_out2[i];
+        cout << t_out2[i];
         fout2 << t_out2[i];
     }
 
-    return 0;
 }
